35c607d613c56f73eb86506c91dbfcb43a37977d
NotPootis <47716620+COkedat@users.noreply.github.com>
2023-11-03 17:23:42 +0900

    필요한거 추가
    바뀔 수 있음

---
+++ img_main.py
@@ -0,0 +1,18 @@
+# 메인맨
+# os랑 넘파이 불러오기
+import os
+import numpy as np
+
+# 학습함수, 테스트함수 불러오기
+from img_train_predict import train_set, predict_set
+
+
+
+def main():
+    input("아무 키나 입력해서 종료")
+    exit()
+
+
+
+if __name__ == "__main__":
+	main()
